740b29cba4c6bf325226afef0c98ff920ebcdc8d
Meera Ramachandran <meeraniranjani@gmail.com>
2019-02-03 14:58:29 -0800

    more files

--- app.js
+++ app.js
@@ -21,3 +21,8 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     modal.find('.modal-body input').val(recipient)
     
   })
+  var formData = new FormData();
+
+formData.append("Sender email", "test@example.com");
+formData.append("Recipient email", "test@example.com"); 
+formData.append("Message", "Hello"); 
